9c3c63e15f31f45b42c47d31fc36389fff32cee2
ambokileB <wholemike2703email.com>
2026-02-22 03:20:58 +0300

    feat:working with vite.config file"

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+
 export default defineConfig({
   plugins: [vue()],
+  base: '/project2/',  // 👈 Weka moja kwa moja! Hii ndiyo unayoitaka
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }, // Funga resolve hapa
-  server: { // Server iwe hapa nje
-    allowedHosts: ['.ngrok-free.app'],
-    host: '0.0.0.0',   // muhimu sana
-    port: 5174,
-    strictPort: true,
   }
 })
-
-
-
-
-
-
